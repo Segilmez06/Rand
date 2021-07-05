f679eadc154a08765d47190c867cf2920d9aba64
Sarp Eren EGILMEZ <2348.sarp.egilmez.2006@gmail.com>
2021-07-05 21:26:08 +0300

    Add source code

---
+++ Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Rand
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            textBox1.Text = trackBar1.Value.ToString();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Random rand = new Random();
+            int val = rand.Next(trackBar1.Value);
+            MessageBox.Show(val.ToString(), "Random number is:");
+        }
+    }
+}
